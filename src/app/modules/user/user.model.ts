09d65955f2241cde3279271f8ff937f155a8f662
md.rahathossenantor <md.rahathossenantor@gmail.com>
2024-03-13 13:39:38 +0000

    user schema and model created

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,1 +1,31 @@
+import { Schema, model } from "mongoose";
+import { TUser } from "./user.interface";
 
+const userSchema = new Schema<TUser>({
+    id: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    needsPasswordChange: {
+        type: Boolean,
+        default: true
+    },
+    role: {
+        type: String,
+        enum: ["admin", "faculty", "student"]
+    },
+    status: {
+        type: String,
+        enum: ["in-progress", "blocked"]
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true });
+
+const User = model<TUser>("User", userSchema);
