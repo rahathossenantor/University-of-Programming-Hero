98599df7d4c31028942e03fb08c8b4ade5c3f1e5
rahathossenantor <md.rahathossenantor@gmail.com>
2024-03-17 06:06:48 +0600

    Controller refactored and code DRYied

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,50 +1,38 @@
-import { NextFunction, Request, Response } from "express";
 import { StudentServices } from "./student.services";
 import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync";
 
 // get all students from students collection
-const getAllStudents = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const students = await StudentServices.getAllStudentsFromDB();
-        res.status(httpStatus.OK).json({
-            success: true,
-            message: "Students retrived successfully",
-            data: students
-        });
-    } catch (err: unknown) {
-        next(err);
-    }
-};
+const getAllStudents = catchAsync(async (req, res) => {
+    const students = await StudentServices.getAllStudentsFromDB();
+    res.status(httpStatus.OK).json({
+        success: true,
+        message: "Students retrived successfully",
+        data: students
+    });
+});
 
 // get single student from students collection
-const getSingleStudent = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { studentId } = req.params;
-        const student = await StudentServices.getSingleStudentFromDB(studentId);
-        res.status(httpStatus.OK).json({
-            success: true,
-            message: (student ? "Student retrived successfully" : "No student found!"),
-            data: student
-        });
-    } catch (err: unknown) {
-        next(err);
-    }
-};
+const getSingleStudent = catchAsync(async (req, res) => {
+    const { studentId } = req.params;
+    const student = await StudentServices.getSingleStudentFromDB(studentId);
+    res.status(httpStatus.OK).json({
+        success: true,
+        message: (student ? "Student retrived successfully" : "No student found!"),
+        data: student
+    });
+});
 
 // delete single student from students collection
-const deleteStudent = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { studentId } = req.params;
-        const dbResponse = await StudentServices.deleteStudentFromDB(studentId);
-        res.status(httpStatus.OK).json({
-            success: true,
-            message: "Student retrived successfully",
-            data: dbResponse
-        });
-    } catch (err: unknown) {
-        next(err);
-    }
-};
+const deleteStudent = catchAsync(async (req, res) => {
+    const { studentId } = req.params;
+    const dbResponse = await StudentServices.deleteStudentFromDB(studentId);
+    res.status(httpStatus.OK).json({
+        success: true,
+        message: "Student retrived successfully",
+        data: dbResponse
+    });
+});
 
 export const StudentControllers = {
     getAllStudents,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,21 +1,17 @@
-import { NextFunction, Request, Response } from "express";
+import catchAsync from "../../utils/catchAsync";
 import { UserServices } from "./user.services";
 import httpStatus from "http-status";
 
 // create new student in students collection
-const createStudent = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { password, student } = req.body;
-        const dbResponse = await UserServices.createStudentIntoDB(password, student);
-        res.status(httpStatus.OK).json({
-            success: true,
-            message: "Student created successfully",
-            data: dbResponse
-        });
-    } catch (err: unknown) {
-        next(err);
-    }
-};
+const createStudent = catchAsync(async (req, res) => {
+    const { password, student } = req.body;
+    const dbResponse = await UserServices.createStudentIntoDB(password, student);
+    res.status(httpStatus.OK).json({
+        success: true,
+        message: "Student created successfully",
+        data: dbResponse
+    });
+});
 
 export const UserControllers = {
     createStudent
