4058e0b4bc594fa238ac66c918694a60b145ebcf
rahathossenantor <md.rahathossenantor@gmail.com>
2024-05-01 17:45:42 +0600

    Academic faculty services created

---
+++ src/app/modules/academicFaculty/academicFaculty.services.ts
@@ -0,0 +1,36 @@
+import { TAcademicFaculty } from "./academicFaculty.interface";
+import { AcademicFaculty } from "./academicFaculty.model";
+
+// create an academic faculty in the database
+const createAcademicFacultyIntoDB = async (payload: TAcademicFaculty) => {
+    const dbRes = await AcademicFaculty.create(payload);
+    return dbRes;
+};
+
+// update academic faculty in database by id
+const updateAcademicFacultyIntoDB = async (
+    id: string,
+    payload: Partial<TAcademicFaculty>,
+) => {
+    const dbRes = await AcademicFaculty.findOneAndUpdate({ _id: id }, payload, { new: true });
+    return dbRes;
+};
+
+// get all academic faculties from the database
+const getAllAcademicFacultiesFromDB = async () => {
+    const dbRes = await AcademicFaculty.find();
+    return dbRes;
+};
+
+// get single academic faculty from the database
+const getSingleAcademicFacultyFromDB = async (id: string) => {
+    const dbRes = await AcademicFaculty.findById(id);
+    return dbRes;
+};
+
+export const AcademicSemesterServices = {
+    createAcademicFacultyIntoDB,
+    updateAcademicFacultyIntoDB,
+    getAllAcademicFacultiesFromDB,
+    getSingleAcademicFacultyFromDB
+};
