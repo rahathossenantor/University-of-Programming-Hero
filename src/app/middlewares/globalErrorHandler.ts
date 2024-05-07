a98bc1575f75745a492c606da441e9a4af9abf1a
rahathossenantor <md.rahathossenantor@gmail.com>
2024-05-07 23:55:08 +0600

    Zod err pattern applied

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,14 +1,47 @@
-import { Request, Response, NextFunction } from "express";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+import { ErrorRequestHandler } from "express";
 import httpStatus from "http-status";
+import { ZodError, ZodIssue } from "zod";
+import { TErrorSource } from "../interface/error";
+import config from "../config";
 
-const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    const statusCode: number = err.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
-    const message: string = err.message || "Something went wrong!";
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    let statusCode: number = err.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
+    let message: string = err.message || "Something went wrong!";
+    let errorSources: TErrorSource = [{
+        path: "",
+        message: "Something went wrong!"
+    }];
+
+    const handleZodError = (err: ZodError) => {
+        const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
+            return {
+                path: issue?.path[issue.path.length - 1],
+                message: issue?.message
+            };
+        });
+        const statusCode: number = 400;
+
+        return {
+            statusCode,
+            message: "Validation error!",
+            errorSources
+        };
+    };
+
+    if (err instanceof ZodError) {
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError?.statusCode;
+        message = simplifiedError?.message;
+        errorSources = simplifiedError?.errorSources;
+    }
 
     return res.status(statusCode).json({
         success: false,
         message,
-        error: err
+        errorSources,
+        stack: config.node_env === "development" ? err?.stack : null
     });
 };
 
