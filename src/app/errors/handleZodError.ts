c5140726c991b1e78f792861cf84384260f51a01
rahathossenantor <md.rahathossenantor@gmail.com>
2024-05-08 18:29:11 +0600

    Handled mongoose validation error

---
+++ src/app/errors/handleZodError.ts
@@ -0,0 +1,22 @@
+import { ZodError, ZodIssue } from "zod";
+import { TErrorSources } from "../interface/error";
+import httpStatus from "http-status";
+
+const handleZodError = (err: ZodError) => {
+    console.log("zooood", err);
+    const errorSources: TErrorSources = err.issues.map((issue: ZodIssue) => {
+        return {
+            path: issue?.path[issue.path.length - 1],
+            message: issue?.message
+        };
+    });
+    const statusCode: number = httpStatus.BAD_REQUEST;
+
+    return {
+        statusCode,
+        message: "Validation error!",
+        errorSources
+    };
+};
+
+export default handleZodError;
--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -2,46 +2,41 @@
 /* eslint-disable no-unused-vars */
 import { ErrorRequestHandler } from "express";
 import httpStatus from "http-status";
-import { ZodError, ZodIssue } from "zod";
-import { TErrorSource } from "../interface/error";
+import { ZodError } from "zod";
+import { TErrorSources } from "../interface/error";
 import config from "../config";
+import handleZodError from "../errors/handleZodError";
+import handleValidationError from "../errors/handleValidationError";
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    // default response data
     let statusCode: number = err.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
     let message: string = err.message || "Something went wrong!";
-    let errorSources: TErrorSource = [{
+    let errorSources: TErrorSources = [{
         path: "",
         message: "Something went wrong!"
     }];
 
-    const handleZodError = (err: ZodError) => {
-        const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
-            return {
-                path: issue?.path[issue.path.length - 1],
-                message: issue?.message
-            };
-        });
-        const statusCode: number = 400;
-
-        return {
-            statusCode,
-            message: "Validation error!",
-            errorSources
-        };
-    };
-
-    if (err instanceof ZodError) {
-        const simplifiedError = handleZodError(err);
+    const assignError = (error, errHandlerFn) => {
+        const simplifiedError = errHandlerFn(error);
         statusCode = simplifiedError?.statusCode;
         message = simplifiedError?.message;
         errorSources = simplifiedError?.errorSources;
+    };
+
+    // handling errors
+    if (err instanceof ZodError) {
+        assignError(err, handleZodError);
+    } else if (err?.name === "ValidationError") {
+        assignError(err, handleValidationError);
     }
 
     return res.status(statusCode).json({
         success: false,
         message,
         errorSources,
-        stack: config.node_env === "development" ? err?.stack : null
+        stack: config.node_env === "development" ? err?.stack : null,
+        // error: err
     });
 };
 
