c646c8441a1241c94506d47378c5bbc3c02bd6e6
rahathossenantor <md.rahathossenantor@gmail.com>
2024-05-11 20:57:40 +0600

    Handled other errors

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -9,6 +9,7 @@ import config from "../config";
 import { ZodError } from "zod";
 import handleCastError from "../errors/handleCastError";
 import handleDuplicateError from "../errors/handleDuplicateError";
+import AppError from "../errors/AppError";
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     // default response data
@@ -41,6 +42,19 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
         assignError(handleCastError);
     } else if (err?.code === 11000) {
         assignError(handleDuplicateError);
+    } else if (err instanceof AppError) {
+        statusCode = err.statusCode;
+        message = err.message;
+        errorSources = [{
+            path: "",
+            message: err.message
+        }];
+    } else if (err instanceof Error) {
+        message = err.message;
+        errorSources = [{
+            path: "",
+            message: err.message
+        }];
     }
 
     return res.status(statusCode).json({
