2e2474ac406df11b8f9cd630af60014438c62caa
rahathossenantor <md.rahathossenantor@gmail.com>
2024-05-14 21:39:38 +0600

    Created faculty interface

---
+++ src/app/modules/faculty/faculty.interface.ts
@@ -0,0 +1,45 @@
+import { Model, Types } from "mongoose";
+
+export type TName = {
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+};
+
+export type TGender = "male" | "female" | "other";
+
+type TBloodGroup =
+    | "A"
+    | "B"
+    | "AB"
+    | "O"
+    | "A+"
+    | "A-"
+    | "B+"
+    | "B-"
+    | "AB+"
+    | "AB-"
+    | "O+"
+    | "O-";
+
+export type TFaculty = {
+  id: string;
+  user: Types.ObjectId;
+  designation: string;
+  name: TName;
+  gender: TGender;
+  dateOfBirth?: Date;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloogGroup?: TBloodGroup;
+  presentAddress: string;
+  permanentAddress: string;
+  avatar?: string;
+  academicDepartment: Types.ObjectId;
+  isDeleted: boolean;
+};
+
+export interface FacultyModel extends Model<TFaculty> {
+  isUserExists(id: string): Promise<TFaculty | null>;
+};
