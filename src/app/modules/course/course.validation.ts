16328a39c05b53e91d9d2498d81bb508cf475777
rahathossenantor <md.rahathossenantor@gmail.com>
2024-06-14 00:26:36 +0600

    Course validation created

---
+++ src/app/modules/course/course.validation.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+
+const PreRequisiteCourseSchema = z.object({
+    course: z.string(),
+    isDeleted: z.boolean().optional()
+});
+
+const CourseCreationValidationSchema = z.object({
+    body: z.object({
+        title: z.string(),
+        prefix: z.string(),
+        code: z.number(),
+        credits: z.number(),
+        preRequisiteCourses: z.array(PreRequisiteCourseSchema).optional(),
+        isDeleted: z.boolean().optional()
+    })
+});
+
+const CourseUpdatationValidationSchema = z.object({
+    body: z.object({
+        title: z.string().optional(),
+        prefix: z.string().optional(),
+        code: z.number().optional(),
+        credits: z.number().optional(),
+        preRequisiteCourses: z
+            .array(PreRequisiteCourseSchema)
+            .optional(),
+        isDeleted: z.boolean().optional()
+    })
+});
+
+export const CourseValidations = {
+    CourseCreationValidationSchema,
+    CourseUpdatationValidationSchema
+};
