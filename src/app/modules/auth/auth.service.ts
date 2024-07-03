d712b08d67b706683f0b3c9df2f212e3c89745e7
rahathossenantor <md.rahathossenantor@gmail.com>
2024-07-03 14:34:42 +0600

    Added some validation on login service

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,7 +1,33 @@
+import httpStatus from "http-status";
+import AppError from "../../errors/AppError";
+import bcrypt from "bcrypt";
+import { User } from "../user/user.model";
 import { TLoginUser } from "./auth.interface";
 
 const loginUser = async (payload: TLoginUser) => {
-    console.log(payload);
+    // checking if the user is exist
+    const user = await User.findOne({ id: payload.id });
+    if (!user) {
+        throw new AppError(httpStatus.NOT_FOUND, "User is not found!");
+    }
+
+    // checking if the user is already deleted
+    const isDeleted = user?.isDeleted;
+    if (isDeleted) {
+        throw new AppError(httpStatus.FORBIDDEN, "This user is deleted!");
+    }
+
+    // checking if the user is blocked
+    const userStatus = user?.status;
+    if (userStatus === "blocked") {
+        throw new AppError(httpStatus.FORBIDDEN, "This user is blocked!");
+    }
+
+    // checking if the password is correct
+    const isPasswordMatched = await bcrypt.compare(payload.password, user.password);
+    if (!isPasswordMatched) {
+        throw new AppError(httpStatus.FORBIDDEN, "Password does not matched!");
+    }
     return payload;
 };
 
