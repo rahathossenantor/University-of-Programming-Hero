a6e550087cbf5d00addbf2b9773db8b8b5227438
rahathossenantor <md.rahathossenantor@gmail.com>
2024-07-08 10:45:18 +0600

    Update user status validation created

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -4,6 +4,13 @@ export const UserValidationSchema = z.object({
     password: z.string().max(20).optional()
 });
 
+export const UpdateUserStatusValidationSchema = z.object({
+    body: z.object({
+        status: z.enum(["in-progress", "blocked"])
+    })
+});
+
 export const UserValidations = {
-    UserValidationSchema
+    UserValidationSchema,
+    UpdateUserStatusValidationSchema
 };
