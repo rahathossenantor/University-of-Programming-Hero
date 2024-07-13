330d150ecded20ba6a10fe548379de4d5d798e10
rahathossenantor <md.rahathossenantor@gmail.com>
2024-07-13 16:20:02 +0600

    Update enrolled course marks validation schema created

--- src/app/modules/enrolledCourse/enrolledCourse.validation.ts
+++ src/app/modules/enrolledCourse/enrolledCourse.validation.ts
@@ -6,6 +6,21 @@ const EnrolledCourseCreationValidationSchema = z.object({
     })
 });
 
+const EnrolledCourseMarksUpdatationValidationSchema = z.object({
+    body: z.object({
+        semesterRegistration: z.string(),
+        offeredCourse: z.string(),
+        student: z.string(),
+        courseMarks: z.object({
+            classTest1: z.number().optional(),
+            midTerm: z.number().optional(),
+            classTest2: z.number().optional(),
+            finalTerm: z.number().optional()
+        })
+    })
+});
+
 export const EnrolledCourseValidations = {
-    EnrolledCourseCreationValidationSchema
+    EnrolledCourseCreationValidationSchema,
+    EnrolledCourseMarksUpdatationValidationSchema
 };
